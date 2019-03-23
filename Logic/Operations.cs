c9a3f5dee257865f1282f41f7127fab71c666ad9
Justyna Hubert <37309986+hubjust@users.noreply.github.com>
2019-03-23 20:02:39 +0100

    more operations for signals

--- Logic/Operations.cs
+++ Logic/Operations.cs
@@ -56,5 +56,85 @@ namespace Logic
 
             return result;
         }
+
+        public static double AvgSignal(List<double> samples, double t1 = 0, double t2 = 0, bool isDiscrete = false)
+        {
+            if (isDiscrete)
+            {
+                return 1.0 / samples.Count * Sum(samples);
+            }
+
+            return 1 / (t2 - t1) * Integral(Math.Abs((t2 - t1) / samples.Count), samples);
+        }
+
+        public static double SignalVariance(List<double> samples, double t1 = 0, double t2 = 0, bool isDiscrete = false)
+        {
+            if (isDiscrete)
+            {
+                return 1.0 / samples.Count * Sum(samples, d => Math.Pow(d - AvgSignal(samples, isDiscrete: true), 2));
+            }
+
+            return 1 / (t2 - t1) * Integral(Math.Abs((t2 - t1) / samples.Count), samples, d => Math.Pow(d - AvgSignal(samples, t1, t2), 2));
+
+        }
+        public static double AbsAvgSignal(List<double> samples, double t1 = 0, double t2 = 0, bool isDiscrete = false)
+        {
+            if (isDiscrete)
+            {
+                return 1.0 / samples.Count * Sum(samples, Math.Abs);
+            }
+
+            return 1 / (t2 - t1) * Integral(Math.Abs((t2 - t1) / samples.Count), samples, Math.Abs);
+        }
+
+        public static double AvgSignalPower(List<double> samples, double t1 = 0, double t2 = 0, bool isDiscrete = false)
+        {
+            if (isDiscrete)
+            {
+                return 1.0 / samples.Count * Sum(samples, d => d * d);
+            }
+
+            return 1 / (t2 - t1) * Integral(Math.Abs((t2 - t1) / samples.Count), samples, d => d * d);
+        }
+
+        public static double RMSSignal(List<double> samples, double t1 = 0, double t2 = 0, bool isDiscrete = false)
+        {
+            if (isDiscrete)
+            {
+                return Math.Sqrt(AvgSignalPower(samples, isDiscrete: true));
+            }
+
+            return Math.Sqrt(AvgSignalPower(samples, t1, t2));
+        }
+
+        private static double Integral(double dx, List<double> samples, Func<double, double> additionalFunc = null)
+        {
+            double integral = 0;
+            foreach (var sample in samples)
+            {
+                if (additionalFunc != null)
+                    integral += additionalFunc(sample);
+                else
+                    integral += sample;
+            }
+
+            integral *= dx;
+
+            return integral;
+        }
+
+        private static double Sum(List<double> samples, Func<double, double> additionalFunc = null)
+        {
+            double sum = 0;
+            foreach (var sample in samples)
+            {
+                if (additionalFunc != null)
+                    sum += additionalFunc(sample);
+                else
+                    sum += sample;
+            }
+
+            return sum;
+        }
     }
 }

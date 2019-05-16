5010de2a513178a912a8bc5752a36fc16f34da93
Karol Podlewski <podlewski@hotmail.com>
2019-05-16 19:30:14 +0200

    Some antenna stuff

---
+++ CyfrowePrzetwarzanieSygnału/View/AntennaWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using ViewModel;
+
+namespace View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class AntennaWindow : Window
+    {
+        public AntennaWindow()
+        {
+            DataContext = new AntennaViewModel();
+            InitializeComponent();
+        }
+    }
+}
---
+++ CyfrowePrzetwarzanieSygnału/ViewModel/AntennaViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using System.Linq;
+
+using Logic;
+using Microsoft.Win32;
+
+namespace ViewModel
+{
+    public class AntennaViewModel : BaseViewModel
+    {
+
+        public ICommand CountCommand { get; set; }
+
+        public double TimeUnit { get; set; }
+        public double RealSpeed { get; set; }
+        public double AbstractSpeed { get; set; }
+        public double SignalPeroid { get; set; }
+        public double SamplingFrequency { get; set; }
+        public double BuffersLength { get; set; }
+        public double ReportingPeroid { get; set; }
+
+        public AntennaViewModel()
+        {
+            CountCommand = new RelayCommand(Count);
+        }
+
+        public void Count()
+        {
+
+        }
+    }
+}
